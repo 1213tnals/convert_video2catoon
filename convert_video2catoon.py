e619041a85112128c7d9870ba90ffc1ee0bc354a
1213tnals <1213tnals@naver.com>
2023-03-26 19:29:04 +0900

    230326 live cam2gray

--- convert_video2catoon.py
+++ convert_video2catoon.py
@@ -1,20 +1,22 @@
 import cv2 as cv
 import numpy as np
 
-video = cv.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 # Get FPS and calculate the waiting time in millisecond
-fps = video.get(cv.CAP_PROP_FPS)
+fps = cap.get(cv.CAP_PROP_FPS)
 wait_msec = int(1 / fps * 1000)
 
 while True:
     # Read an image from 'video'
-    valid, img = video.read()
-    if not valid:
+    ret, video = cap.read()
+    if not ret:
         break
 
+    video_gray = cv.cvtColor(video, cv.COLOR_BGR2GRAY)
+
     # Show the image
-    cv.imshow('Video Player', img)
+    cv.imshow('Video Player', video_gray)
 
     # Terminate if the given key is ESC
     key = cv.waitKey(wait_msec)
